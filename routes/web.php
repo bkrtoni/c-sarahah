339378ac36184f2cad78656a8caaf9a1d2b1a371
bkrtoni <bakerzaher@gmail.com>
2018-04-05 22:33:15 +0300

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+// Sarahah Routes
+
+Route::get('/', 'PageController@index');
+Route::get('/about', 'PageController@about');
+Route::get('/contact', 'PageController@contact');
+
+Route::get('messages','AccountController@index');
+
+Route::get('user/{username}','AccountController@show');
+
+Route::get('thankyou','PageController@thank');
+Route::post('make/{id}','AccountController@store');
+
+Route::delete('messages','AccountController@delete');
+
+Route::get('setting','SettingController@index');
+Route::get('setting/password','SettingController@pass');
+Route::get('setting/delete','SettingController@del');
+Route::post('setting/delete','SettingController@delete');
+Route::post('setting/{op}','SettingController@store');
+
+
+Auth::routes();
+
